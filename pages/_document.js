7121791458fe5d9e14059d41f31d790a0f2e1185
Syed Fazle Rahman <fazle@hashnode.com>
2018-05-03 18:30:57 +0530

    Fix font issue

--- pages/_document.js
+++ pages/_document.js
@@ -20,7 +20,6 @@ export default class Hashnews extends Document {
             <meta charset="utf-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
             <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous" />
-            <link rel="stylesheet" href="https://i.icomoon.io/public/temp/67ab53efbb/Hashnews/style.css"/>
             <link rel="stylesheet" href="/static/css/app.css"/>
         </Head>
         <body>
