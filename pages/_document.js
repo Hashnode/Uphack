7815a9bd180556763acebfd4bf36d78f041b72e0
Sandeep <sandeep@devmag.io>
2018-05-31 18:24:31 +0530

    Change case of crossorigin and charset

--- pages/_document.js
+++ pages/_document.js
@@ -19,9 +19,9 @@ export default class Hashnews extends Document {
       <html>
         <Head>
             {this.props.helmet.title.toComponent()}
-            <meta charset="utf-8" />
+            <meta charSet="utf-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous" />
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossOrigin="anonymous" />
             <link rel="stylesheet" href="//cdn.materialdesignicons.com/2.4.85/css/materialdesignicons.min.css"/>
             <link rel="stylesheet" href="/static/css/app.css"/>
             <link rel="icon" href="/static/images/fav.png" type="image/png"/>
