474e5856d2308e9719f95ee527a9147f4fc3bf2a
Sandeep <sandeep@devmag.io>
2018-05-31 15:15:36 +0530

    Comment out stuff in nav

--- components/nav.js
+++ components/nav.js
@@ -52,9 +52,9 @@ class Nav extends React.Component {
                                 <li className="list-inline-item">
                                     <a href="/submit">Submit</a>
                                 </li>
-                                <li className="list-inline-item">
+                                {/* <li className="list-inline-item">
                                     <a href="#" title="Switch Theme"><i className="mdi mdi-weather-night"></i></a>
-                                </li>
+                                </li> */}
                             </ul>
                         </div>
                         <div className="d-flex flex-row align-items-center">
